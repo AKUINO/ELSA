96b2f02ddf067535942529b6c1333743f73e8900
MaximeFranco <franco.maxime@gmail.com>
2017-03-21 09:37:36 +0100

    correction Bug N°1 : problème for

--- ConfigurationELSA.py
+++ ConfigurationELSA.py
@@ -402,7 +402,7 @@ class UpdateThread(threading.Thread):
 	    now = int(time.time())
 	    self.config.InfoSystem.updateInfoSystem(now)
 	    if not len(self.config.AllSensors.elements) == 0 :
-		for k,sensor in self.config.AllSensors.items():
+		for k,sensor in self.config.AllSensors.elements.items():
 		    if sensor.fields['channel'] == wire:
 			try:
 			    aDevice = ow.Sensor('/'+sensor.fields['sensor'])
