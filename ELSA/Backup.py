ed237c237c2b151a2ae62b8514b342f124ccf749
Quentin Peten <Quentin Peten @ mail.com>
2018-02-21 16:57:43 +0100

    Configure dirs logically

--- ELSA/Backup.py
+++ ELSA/Backup.py
@@ -4,6 +4,7 @@ from shutil import make_archive
 from contextlib import contextmanager
 import os
 import datetime
+import ConfigurationELSA as elsa
 
 @contextmanager
 def cd(newdir):
@@ -14,16 +15,16 @@ def cd(newdir):
     finally:
         os.chdir(prevdir)
 
+
 today = datetime.datetime.today()
 
 archive_output_file_name = today.strftime("backup-%Yy%mm%dd-%Hh%Mm%Ss")
-archive_type = "zip" #Can be tar, bztar, gztar
-archive_output_dir = os.path.abspath("/home/pi/akuino/ELSA/ELSA/static/temp/")
-archive_input_dir = os.path.abspath("/home/pi/akuino/ELSA/data/")
+archive_type = "zip"  # Can be tar, bztar, gztar
+archive_output_dir = os.path.abspath(elsa.DIR_WEB_TEMP)
+archive_input_dir = os.path.abspath(elsa.DIR_USER_DATA)
 
 # Switching working directory to save to the right place
 # A context is used to guarantee we will go back to the original place afterwards
 with cd(archive_output_dir):
-    make_archive(archive_output_file_name, archive_type, archive_output_dir, archive_input_dir)
-
-
+    make_archive(archive_output_file_name, archive_type,
+                 archive_output_dir, archive_input_dir)
