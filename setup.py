e5064e27642f7be75b0496cc7b3aab12360cd968
Quentin Peten <Quentin Peten @ mail.com>
2018-02-21 11:23:55 +0100

    Added initial setup.ini

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="ELSA",
+    version="0.1",
+    author="Destin-informatique",
+    author_email="christophe.dupriez@destin-informatique.com",
+    url="http://www.destin-informatique.com",
+    description="Monitoring de la production par lots",
+    long_description=read('README.md'),
+    packages=find_packages(),
+    classifiers = [
+        'Programming Language :: Python',
+        ]
+)
