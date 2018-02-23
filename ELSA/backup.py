d2eab7f8a6d383cb46d9ace65197d0c079c9fded
Quentin Peten <Quentin Peten @ mail.com>
2018-02-23 10:26:59 +0100

    Fixed path inside backup archive

--- ELSA/backup.py
+++ ELSA/backup.py
@@ -25,13 +25,13 @@ def create_backup_zip():
     """Creates an archive of the DIR_USER_DATA and saves it to DIR_WEB_TEMP. Returns absolute path to the generated file."""
     today = datetime.datetime.today()
     
-    archive_output_file_name = today.strftime(ARCHIVE_FILE_NAME_PREFIX + ARCHIVE_FILE_NAME_DATE_FORMAT + ARCHIVE_FILE_NAME_SUFFIX)
     archive_output_dir = os.path.abspath(elsa.DIR_WEB_TEMP)
     archive_input_dir = os.path.abspath(elsa.DIR_USER_DATA)
+    archive_output_file_name = today.strftime(ARCHIVE_FILE_NAME_PREFIX + ARCHIVE_FILE_NAME_DATE_FORMAT + ARCHIVE_FILE_NAME_SUFFIX)
+    archive_output_file_name = os.path.join(archive_output_dir, archive_output_file_name)
 
     # Switching working directory to save to the right place
     # A context is used to guarantee we will go back to the original place afterwards
-    with cd(archive_output_dir):
-        make_archive(archive_output_file_name, ARCHIVE_TYPE,
-                     archive_output_dir, archive_input_dir)
-    return os.path.join(archive_output_dir, archive_output_file_name) + '.' + ARCHIVE_TYPE
+    with cd(archive_input_dir):
+        make_archive(archive_output_file_name, ARCHIVE_TYPE)
+    return os.path.join(archive_output_dir, archive_output_file_name + '.' + ARCHIVE_TYPE)
