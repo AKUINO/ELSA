08d3a9234c3eb38ef9011fd4574e7183511a6f2d
Christophe Dupriez <dupriez@destin.be>
2018-07-03 16:03:02 +0200

    Bug add DM

--- ELSA/ConfigurationELSA.py
+++ ELSA/ConfigurationELSA.py
@@ -2579,7 +2579,7 @@ class CheckPoint(Group):
                     self.dm.insert(i, model.getID())
                     break
             if insert is False:
-                self.dm.append(self.dm.getID())
+                self.dm.append(model.getID())
 
     def remove_vm(self, model):
         if model.fields['vm_id'] in self.vm:
