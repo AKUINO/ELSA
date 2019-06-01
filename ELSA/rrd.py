b86ef6de44ecf2fc853e1d0cc5afa99be7d0f56e
Christophe Dupriez <dupriez@destin.be>
2019-06-01 10:31:58 +0200

    No hole in Grafana series...

--- ELSA/rrd.py
+++ ELSA/rrd.py
@@ -18,14 +18,13 @@ def get_datapoints_from_s_id(sensorID, time_from_utc, time_to_utc):
     time_to = calendar.timegm(time_to_utc)
     
     result = rrdtool.fetch(str(path), 'AVERAGE', '--start=' + str(time_from), '--end=' + str(time_to))
-    
+    values = result[2]
     
     datapoints = []
     rrd_step = result[0][2]
-    for i in range(0, len(result[2])):
-        val = result[2][i][0]
-        if val != nan:
+    for i in range(0, len(values)):
+        val = values[i][0]
+        if val is not None and val != nan:
             datapoints.append([val, (time_from + i*rrd_step)*1000])
-    
 
     return datapoints
